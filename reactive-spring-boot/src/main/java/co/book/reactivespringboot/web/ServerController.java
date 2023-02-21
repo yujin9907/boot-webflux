feb5dc7b308c52513d87bad51ff1529369da3b97
yujin9907 <heoyujin07@gmail.com>
2023-02-21 19:29:15 +0900

    start web app

---
+++ reactive-spring-boot/src/main/java/co/book/reactivespringboot/web/ServerController.java
@@ -0,0 +1,25 @@
+package co.book.reactivespringboot.web;
+
+import javax.print.attribute.standard.MediaTray;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.book.reactivespringboot.entity.Dish;
+import co.book.reactivespringboot.service.KitchenService;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+@RequiredArgsConstructor
+@RestController
+public class ServerController {
+
+    private final KitchenService kitchenService;
+
+    @GetMapping(value = "/index", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Dish> testIndex() {
+        return this.kitchenService.getDishes();
+    }
+
+}
